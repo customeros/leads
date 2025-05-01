c4d9256a272aac113b6da72d4e417be59154d59e
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-05-01 17:51:29 +0100

    patch: fix model (#16)

--- internal/models/company.go
+++ internal/models/company.go
@@ -15,7 +15,6 @@ type Company struct {
 	LastEngagementSource  enum.AdPlatform           `gorm:"column:last_engagement_source;type:varchar(50)"`
 	TopEngagementChannel  enum.Channel              `gorm:"column:top_engagement_channel;type:varchar(50)"`
 	IdentifiedContacts    int                       `gorm:"column:identified_contacts;type:varchar(50)"`
-	AnonymousVisitors     int                       `gorm:"column:anonymous_visitors:varchar(50)"`
 	Stage                 enum.CustomerJourneyStage `gorm:"column:stage;type:varchar(50);index;default:'target'"`
 	StageEngagementCount  int                       `gorm:"column:stage_engagement_count;type:integer;default:0"`
 	FirstEngagementAt     *time.Time                `gorm:"column:first_engagement_at;type:timestamptz"`
